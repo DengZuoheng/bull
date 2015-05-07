8b8657b4ebd71e49eb8596055eaabcc542319b75
DengZuoheng <dengzuoheng@gmail.com>
2015-05-07 15:37:02 +0800

    适配成VS项目

--- bull/test/test_wencaispider.py
+++ bull/test/test_wencaispider.py
@@ -6,19 +6,16 @@ path = sys.path[0]
 parent_path = os.path.dirname(path) 
 sys.path.insert(0,(parent_path))
 
-
 from service.spider import Spider
 from service.wencaispider import WencaiSpider
-
 import unittest
 
-class spidertest(unittest.TestCase):
-    def testWencaiSpider(self):
+class Test_test_wencaispider(unittest.TestCase):
+    def test_WencaiSpider(self):
         spider = WencaiSpider('http://www.iwencai.com/stockpick')
         r = []
         r = spider.results()
-        if(len(r)<2000):
-            raise Exception('wrong total')
+        self.assertGreater(len(r),2000)
 
-if __name__ =='__main__':  
-    unittest.main()  
+if __name__ == '__main__':
+    unittest.main()
