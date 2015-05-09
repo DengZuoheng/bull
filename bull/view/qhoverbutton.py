c10cbf645c3f5165e16681c9ae42a97b63af1dd6
DengZuoheng <dengzuoheng@gmail.com>
2015-05-09 11:35:53 +0800

    add qhoverbutton
    就是右上角那关闭按钮

---
+++ bull/view/qhoverbutton.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python  
+#-*-coding:utf-8-*-
+from PyQt4 import QtGui
+from PyQt4 import Qt  
+from PyQt4 import QtCore  
+
+class QHoverButton(QtGui.QWidget):
+    def __init__(self,parent, image, image_active, width=24, height=24):
+        super(QHoverButton, self).__init__(parent)
+        self.image = image
+        self.image_active = image_active
+        self.__hover = False
+        self.width = width
+        self.height = height
+        self.setMouseTracking(True)
+        self.setMaximumSize(width,height)
+        self.installEventFilter(self)
+         
+    def paintEvent(self,e):
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        self.__drawWidget(painter)
+        painter.end()
+
+    def __drawWidget(self,painter):
+        if(self.__hover):
+            painter.drawImage(
+                QtCore.QPoint(0,0),self.image_active)
+        else:
+            painter.drawImage(
+                QtCore.QPoint(0,0),self.image)
+
+    def eventFilter(self, qobject, qevent):
+        qtype = qevent.type()
+        if qtype == QtCore.QEvent.Enter:
+            self.__hover = True
+            self.update()
+        if qtype == QtCore.QEvent.Leave:
+            self.__hover = False
+            self.update()
+        if qtype == QtCore.QEvent.MouseButtonPress:
+            self.emit(QtCore.SIGNAL('clicked()'))
+        
+        return super(QHoverButton, self).eventFilter(qobject, qevent)
+    
+    def __in_btn_area(self,point):
+        if 0 <= point.x() <= self.width and 0 <= point.y() <= self.height:
+            return True
+        return False
