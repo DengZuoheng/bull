b51251d442805ffa7dab0d1ed16aca28489e83b3
DengZuoheng <dengzuoheng@gmail.com>
2015-05-11 22:10:16 +0800

    fixed q distribution slider

--- bull/view/qdistributionslider.py
+++ bull/view/qdistributionslider.py
@@ -4,83 +4,7 @@ import sys
 from PyQt4 import QtCore,QtGui
 from qrangeslider import QRangeSlider
 
-import pycurl
-import StringIO
-import urllib
-import re
-import json
-
-class example_spider():
-    def __init__(self, base_url='http://www.iwencai.com/stockpick'):
-        
-        self.base_url = base_url
-        self.buf_1 = StringIO.StringIO()
-        self.buf_2 = StringIO.StringIO()
-        self.buf_3 = StringIO.StringIO()
-        self.curl = pycurl.Curl()
-        self.curl.setopt(pycurl.CONNECTTIMEOUT,5)
-        self.curl.setopt(pycurl.TIMEOUT,50)
-        self.curl.setopt(pycurl.COOKIEFILE,'')
-        self.curl.setopt(pycurl.FAILONERROR,True)
-        self.save = []
-
-    def result(self):
-        buf1 = StringIO.StringIO()
-        buf2 = StringIO.StringIO()
-        c=pycurl.Curl()
-        base_url = 'http://www.iwencai.com/stockpick'
-        values = {
-            'typed':'1',
-            'preParams':'',
-            'ts':'1',
-            'f':'1',
-            'qs':'1',
-            'selfsectsn':'',
-            'querytype':'',
-            'searchfilter':'',
-            'tid':'stockpick',
-            'w':'pe',
-        }
-        
-        #第一次请求获取含token的JSON
-        url = self.base_url+'/search?%s'%(urllib.urlencode(values))
-        self.curl.setopt(pycurl.URL, url)
-        self.curl.setopt(pycurl.WRITEFUNCTION, self.buf_1.write)#设置回调
-        self.curl.perform()
-        
-        res = re.findall(u'var allResult = (.*)?;',self.buf_1.getvalue())
-        assert 1 == len(res)#应该只有一个符合结果
-        token_obj = json.loads(res[0])
-        
-        #获取含token后请求一次拉取所有股票
-        args = {
-            'token': token_obj['token'],#token
-            'p':1,#第几页
-            'perpage':token_obj['total'],#每页多少股票
-        }
-        url = self.base_url+'/cache?%s'%(urllib.urlencode(args))
-        self.curl.setopt(pycurl.URL, url)
-        self.curl.setopt(pycurl.WRITEFUNCTION, self.buf_2.write)#设置回调
-        self.curl.perform()
-        
-        response = self.buf_2.getvalue()
-        all_result = json.loads(response)
-        data = all_result['list']
-
-        #print all_result['list']
-        return data
-
-def example_result():
-    sp = example_spider()
-    ret = sp.result()
-    l = []
-    for item in ret:
-        if item[3]=='--':
-            continue
-        l.append(float(item[3]))#现价
-    return l
-
-class QDistributionSlider(QtGui.QWidget):
+class QDistributionSlider(QtGui.QFrame):
     def __init__(self,
             data,
             data_max,
@@ -104,18 +28,18 @@ class QDistributionSlider(QtGui.QWidget):
         self.__initUI()
 
     def __uniformization(self):
-        dis = [0]*self.range_slider_width
+        dis = [0]*(self.range_slider_width+1)
         e = float((self.data_max - self.data_min))/self.range_slider_width
         for i in self.data:
-            idx = int(i/e) -2
-            dis[idx-1] = dis[idx-1]+1
+            idx = int((i-self.data_min)/e)
+            dis[idx] = dis[idx]+1
         max_value = max(dis)
         for i in range(len(dis)):
-            dis[i] = float(dis[i]/max_value)
+            dis[i] = float(float(dis[i])/max_value)
         return dis
 
     def __initUI(self):
-        self.setMinimumSize(self.width,self.height)
+        self.setFixedSize(self.width,self.height)
         dis = self.__uniformization()
         self.range_slider = QRangeSlider(
             width=self.range_slider_width,
@@ -140,10 +64,7 @@ class QDistributionSlider(QtGui.QWidget):
         validator = QtGui.QRegExpValidator(regexp)
         self.left_edit.setValidator(validator)
         self.right_edit.setValidator(validator)
-
-        self.left_edit.setText('%f'%(self.lvalue*self.data_max))
-        self.right_edit.setText('%f'%(self.rvalue*self.data_max))
-
+        self.__set_text(0,1)
         hbox = QtGui.QHBoxLayout()
         hbox.setContentsMargins(0, 0, 0, 0)
         hbox.addWidget(self.left_edit)
@@ -190,10 +111,14 @@ class QDistributionSlider(QtGui.QWidget):
         self.right_edit.clearFocus()
 
     def changeRange(self,lvalue,rvalue):
-        self.lvalue = lvalue
-        self.rvalue = rvalue
-        self.left_edit.setText('%f'%(lvalue*self.data_max))
-        self.right_edit.setText('%f'%(rvalue*self.data_max))
+        self.__set_text(lvalue,rvalue)
+
+    def __set_text(self,lvalue,rvalue):
+        _left_text = lvalue*(self.data_max-self.data_min)+self.data_min
+        _right_text = rvalue*(self.data_max-self.data_min)+self.data_min
+        self.left_edit.setText('%f'%_left_text)
+        self.right_edit.setText('%f'%_right_text)
+
 
 
 class Example(QtGui.QWidget):
@@ -202,7 +127,7 @@ class Example(QtGui.QWidget):
         self.initUI()
 
     def initUI(self):
-        ret = example_result()
+        ret = [1,4,6,4,2,5,9,4,9]
         max_value = max(ret)
         min_value = min(ret)
         self.wid = QDistributionSlider(ret,max_value,min_value)
