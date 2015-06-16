ae9524bd6a95869c4c2f97e493a5ba64024e8488
1040870658 <1040870658@qq.com>
2015-06-16 13:49:36 +0800

    updated xueqiu_spider & test

---
+++ bull/test/test_xueqiuspider.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+path = sys.path[0] 
+parent_path = os.path.dirname(path) 
+sys.path.insert(0,(parent_path))
+
+from service.spider import Spider
+from service.xueqiu_spider import XueqiuSpider
+from model.stock import Stock
+import unittest
+
+class Test_test_xueqiuspider(unittest.TestCase):
+    def test_XueqiuSpider(self):
+        spider = XueqiuSpider()
+        r = []
+        r = spider.results()
+        self.assertGreater(len(r),2000)
+        if not isinstance(r[0],Stock):
+            raise Exception('type error: result item is not Stock instance')
+
+if __name__ == '__main__':
+    unittest.main()
